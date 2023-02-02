1ee48e881b4ad052166c256a051152aa800561eb
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2023-02-02 19:54:37 +0100

    lets begin

--- src/main/java/org/airbus/adapter/TwoPinDevice.java
+++ src/main/java/org/airbus/adapter/TwoPinDevice.java
@@ -1,8 +1,5 @@
 package org.airbus.adapter;
 
-public class TwoPinDevice implements IDevice{
-    @Override
-    public void twoPinPlugIn() {
-        System.out.println("two pin device working...");
-    }
+public class TwoPinDevice {
+
 }
